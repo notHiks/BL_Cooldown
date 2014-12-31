384f88d11f8aea3db8198c11483f4651d2daf476
funkydude <funkydude@5c890ef7-4dad-46ac-8614-210ac39223ea>
2014-12-31 00:26:17 +0000

    Another attempt at fixing bad LibCandyBar usage.

--- core.lua
+++ core.lua
@@ -344,55 +344,56 @@ function BLCD:CreateCooldown(index, cooldown)
 	end)
 
 	local function CleanBar(callback, bar)
-	local a = bar:Get("raidcooldowns:anchor") --'a' is frameicon
-	if a and a.bars and a.bars[bar] then
-		a.bars[bar] = nil
+		local a = bar:Get("raidcooldowns:anchor") --'a' is frameicon
+		if a and a.bars and a.bars[bar] then
+			a.bars[bar] = nil
 
-		local bd = bar.candyBarBackdrop
-		bd:Hide()
-		if bd.iborder then
-			bd.iborder:Hide()
-			bd.oborder:Hide()
-		end
-		--bar:SetTexture(nil)
-		local guid = bar:Get("raidcooldowns:key")
-		local spell = bar:Get("raidcooldowns:spell")
-		local cooldown = bar:Get("raidcooldowns:cooldown")
-		local caster = bar:Get("raidcooldowns:caster")
-		--[[if BLCD['handles'] and BLCD["handles"][guid] and BLCD["handles"][guid][spell] then
-			BLCD['handles'][guid][spell] = nil
-		end]]
-		BLCD.curr[cooldown['spellID']][guid] = nil;
+			local bd = bar.candyBarBackdrop
+			bd:Hide()
+			if bd.iborder then
+				bd.iborder:Hide()
+				bd.oborder:Hide()
+			end
+			--bar:SetTexture(nil)
+			local guid = bar:Get("raidcooldowns:key")
+			local spell = bar:Get("raidcooldowns:spell")
+			local cooldown = bar:Get("raidcooldowns:cooldown")
+			local caster = bar:Get("raidcooldowns:caster")
+			--[[if BLCD['handles'] and BLCD["handles"][guid] and BLCD["handles"][guid][spell] then
+				BLCD['handles'][guid][spell] = nil
+			end]]
+			BLCD.curr[cooldown['spellID']][guid]
+			BLCD.curr[cooldown['spellID']][guid] = nil;
 
-		if(BLCD.db.profile.cdannounce and BLCD.dead[caster] == 0) then
-			local name = select(1, GetSpellInfo(cooldown['spellID']))
-			local message = caster.."'s ".. GetSpellLink(cooldown['spellID']).. " is ready!"
-			if BLCD.db.profile.announcechannel then
-				local list = {GetChannelList()}
-				local channel = BLCD.db.profile.customchan
-				for i = 1,#list/2 do
-					if list[i*2] == channel then
-						SendChatMessage(message ,"CHANNEL", nil, list[(i*2)-1]);
+			if(BLCD.db.profile.cdannounce and BLCD.dead[caster] == 0) then
+				local name = select(1, GetSpellInfo(cooldown['spellID']))
+				local message = caster.."'s ".. GetSpellLink(cooldown['spellID']).. " is ready!"
+				if BLCD.db.profile.announcechannel then
+					local list = {GetChannelList()}
+					local channel = BLCD.db.profile.customchan
+					for i = 1,#list/2 do
+						if list[i*2] == channel then
+							SendChatMessage(message ,"CHANNEL", nil, list[(i*2)-1]);
+						end
 					end
+				elseif IsInRaid() or IsInGroup(2) then
+					SendChatMessage(message ,IsInGroup(2) and "INSTANCE_CHAT" or "RAID");
+				elseif IsInGroup() then
+					SendChatMessage(message ,"PARTY");
+				else
+					SendChatMessage(message ,"SAY");
 				end
-			elseif IsInRaid() or IsInGroup(2) then
-				SendChatMessage(message ,IsInGroup(2) and "INSTANCE_CHAT" or "RAID");
-			elseif IsInGroup() then
-				SendChatMessage(message ,"PARTY");
-			else
-				SendChatMessage(message ,"SAY");
 			end
-		end
 
-		if BLCD.db.profile.availablebars and BLCD.db.profile.cooldown[cooldown.name] and a:IsVisible() then
-			local unitalive = (not UnitIsDeadOrGhost(caster) and UnitIsConnected(caster))
-			if BLCD.cooldownRoster[cooldown['spellID']][guid] and unitalive then
-				BLCD:CreatePausedBar(cooldown,guid)
+			if BLCD.db.profile.availablebars and BLCD.db.profile.cooldown[cooldown.name] and a:IsVisible() then
+				local unitalive = (not UnitIsDeadOrGhost(caster) and UnitIsConnected(caster))
+				if BLCD.cooldownRoster[cooldown['spellID']][guid] and unitalive then
+					BLCD:CreatePausedBar(cooldown,guid)
+				end
 			end
+			BLCD:RearrangeBars(a)
+			a.text:SetText(BLCD:GetTotalCooldown(cooldown['spellID']))
 		end
-		BLCD:RearrangeBars(a)
-		a.text:SetText(BLCD:GetTotalCooldown(cooldown['spellID']))
-	end
 	end
 
 	CB.RegisterCallback(self, "LibCandyBar_Stop", CleanBar)
@@ -503,8 +504,6 @@ function BLCD:CreatePausedBar(cooldown,guid)
 		local bar = BLCD:CreateBar(nil, cooldown, caster, cooldownFrameicons[spellID], guid, duration - 1, spellName)
 		BLCD.curr[spellID][guid] = bar
 		bar:SetTimeVisibility(false)
-		bar.candyBarBar:SetMinMaxValues(0, bar.remaining)
-		bar.candyBarBar:SetValue(bar.remaining)
 		bar:Start()
 		bar.updater:Pause()
 		bar:EnableMouse(true)
@@ -1374,7 +1373,8 @@ function BLCD:RearrangeBars(anchor) -- frameicon
 		if anchor:IsVisible() then
 			BLCD:BLHeight(anchor:GetParent(), 28*BLCD.db.profile.scale)
 		end
-	return end
+		return
+	end
 	local frame = anchor:GetParent()
 	local scale = BLCD.db.profile.scale
 	local growth = BLCD.db.profile.growth
@@ -1385,8 +1385,6 @@ function BLCD:RearrangeBars(anchor) -- frameicon
 			currBars[#currBars + 1] = bar
 		else
 			--print('hidden', bar:Get("raidcooldowns:caster"), bar:Get("raidcooldowns:spell"))
-			bar:Show()
-			anchor.bars[bar] = nil
 			bar:Stop()
 		end
 	end
